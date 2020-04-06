20510c0308b34a525420f37518aaeb2460f0d53e
Popky511 <62186253+Popky511@users.noreply.github.com>
2020-04-06 08:58:33 +0200

    Add files via upload

--- oop.php
+++ oop.php
@@ -5,22 +5,28 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 //abstraktní trida ustredny a v ni protected $napetí
 abstract class  ustredny_Pokorny_Jakub{
     
     protected $napeti_Pokorny_Jakub;
-    //nastavení napeti
-    public function setNapeti_Pokorny_Jakub($napeti_Pokorny_Jakub){
+    /**
+     * paramert muze byt jen float
+     * kontrola taky jen float
+     * pri cteni to samé
+ */
+    public function setNapeti_Pokorny_Jakub(float $napeti_Pokorny_Jakub) : float{
         $this->napeti_Pokorny_Jakub = $napeti_Pokorny_Jakub;
     }
     //precteni napeti
-    public function getNapeti_Pokorny_Jakub() {
+    public function getNapeti_Pokorny_Jakub() : float {
         return $this-> napeti_Pokorny_Jakub;
     }
 }
 // class ustredna se dedi do abs. class ustredny
 class  ustredna_Pokorny_Jakub extends ustredny_Pokorny_Jakub{
    public $Napeti_Pokorny_Jakub= 15;
+   
    //nastaveni konstanty type
    const TYPE ="12";
 }
@@ -32,5 +38,5 @@ interface iustredna_Pokorny_Jakub extends ustredny_Pokorny_Jakub{
 
 //pres vardummp vypise hodnotu v type a hodnotu napeti na ustredne
  var_dump (ustredna_Pokorny_Jakub::TYPE);
- var_dump ($Napeti_Pokorny_Jakub);
+ var_dump (ustredna_Pokorny_Jakub::Napeti_Pokorny_Jakub);
 ?> 
