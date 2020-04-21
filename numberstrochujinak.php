0459681b0c544c2777f035425efe9af845e8e066
Popky511 <62186253+Popky511@users.noreply.github.com>
2020-04-21 20:33:13 +0200

    Add files via upload
    tohle je upravená verze

--- numberstrochujinak.php
+++ numberstrochujinak.php
@@ -5,65 +5,89 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/*
+ * @autor Jakub Pokorny
+ */
+//nastaveni tridy numbers
 class numbersPokorny{
+    //staticka fuknce round
     static public function roundPokorny($zaokrouhli) {
         return round($zaokrouhli);
         
     }
+    //staticka fuknce ceil
    static public function ceilPokorny($zaokrouhlinahoru) {
        return ceil($zaokrouhlinahoru);
    }
+   //staticka fuknce floor
    static public function floorPokorny($zaokrouhlidolu) {
        return floor($zaokrouhlidolu);
    }
+   //staticka fuknce sqrt
    static public function sqrtPokorny($druhaodmocnina) {
        return sqrt($druhaodmocnina);
    }
+   //staticka fuknce pow
    static public function powPokorny($mocnina,$mocnitel) {
        return pow($mocnina, $mocnitel);
    }
+   //staticka fuknce sin
    static public function sinPokorny($uhel) {
        return sin($uhel);
    }
+   //staticka fuknce cos
    static public function cosPokorny($uhel) {
        return cos($uhel);
 }
+//staticka fuknce tan
 static public function tanPokorny($uhel) {
        return tan($uhel);
 }
+//staticka fuknce asin
 static public function asinPokorny($zcislanauhel) {
        return asin($zcislanauhel);
    }
+   //staticka fuknce acos
    static public function acosPokorny($zcislanauhel) {
        return acos($zcislanauhel);
 }
+//staticka fuknce atan
 static public function atanPokorny($zcislanauhel) {
        return atan($zcislanauhel);
 }
+//staticka fuknce fmod
 static public function fmodPokorny($zaklad,$delitel) {
     return fmod($zaklad, $delitel);
 }
+//staticka fuknce max
 static public function maxPokorny($pole) {
     return max($pole);
 }
+//staticka fuknce min
 static public function minPokorny($pole) {
     return min($pole);
 }
+//staticka fuknce abs
 static public function absPokorny($abscislo) {
     return abs($abscislo);
 }
+//staticka fuknce odcitani
 static public function odecitaniPokorny($a,$b) {
     return $a-$b;
 }
+//staticka fuknce scitani
 static public function scitaniPokorny($a,$b) {
     return $a+$b;
 }
+//staticka fuknce deleni
 static public function deleniPokorny($a,$b) {
     return $a/$b;
 }
+//staticka fuknce nasobeni
 static public function nasobeniPokorny($a,$b) {
     return $a*$b;
 }
+//staticka fuknce formatovani
 static public function number_formatPokorny($formatuje) {
     return number_format($formatuje);
 }
@@ -83,38 +107,45 @@ $abscislo = 57.12;
 $a = 8;
 $b = 2;
 $formatuje = 10;
+//vypíše informace o stringu a funkci round
 var_dump(numbersPokorny::roundPokorny($zaokrouhli)); 
+//vypíše informace o stringu a funkci ceil
 var_dump(numbersPokorny::ceilPokorny($zaokrouhlinahoru));
+//vypíše informace o stringu a funkci floor
 var_dump(numbersPokorny::floorPokorny($zaokrouhlidolu));
+//vypíše informace o stringu a funkci sqrt
 var_dump(numbersPokorny::sqrtPokorny($druhaodmocnina));
+//vypíše informace o stringu a funkci pow
 var_dump(numbersPokorny::powPokorny($mocnina, $mocnitel));
+//vypíše informace o stringu a funkci sininus
 var_dump(numbersPokorny::sinPokorny($uhel));
+//vypíše informace o stringu a funkci cosinus
 var_dump(numbersPokorny::cosPokorny($uhel));
+//vypíše informace o stringu a funkci tangens
 var_dump(numbersPokorny::tanPokorny($uhel));
+//vypíše informace o stringu a funkci asin
 var_dump(numbersPokorny::asinPokorny($zcislanauhel));
+//vypíše informace o stringu a funkci acos
 var_dump(numbersPokorny::acosPokorny($zcislanauhel));
+//vypíše informace o stringu a funkci atan
 var_dump(numbersPokorny::atanPokorny($zcislanauhel));
+//vypíše informace o stringu a funkci fmod
 var_dump(numbersPokorny::fmodPokorny($zaklad, $delitel));
+//vypíše informace o stringu a funkci max
 var_dump(numbersPokorny::maxPokorny($pole));
+//vypíše informace o stringu a funkci min
 var_dump(numbersPokorny::minPokorny($pole));
+//vypíše informace o stringu a funkci abs
 var_dump(numbersPokorny::absPokorny($abscislo));
+//vypíše informace o stringu a funkci odcitani
 var_dump(numbersPokorny::odecitaniPokorny($a, $b));
+//vypíše informace o stringu a funkci scitani
 var_dump(numbersPokorny::scitaniPokorny($a, $b));
+//vypíše informace o stringu a funkci deleni
 var_dump(numbersPokorny::deleniPokorny($a, $b));
+//vypíše informace o stringu a funkci nasobeni
 var_dump(numbersPokorny::nasobeniPokorny($a, $b));
+//vypíše informace o stringu a funkci number_format
 var_dump(numbersPokorny::number_formatPokorny($formatuje));
 
-// * floor ($mensi)
- //* sqrt(2odmocnina)
- //* pow(cislo, mocnina)
- //* sin(cislo), cos(cislo), tan(cislo)
- //* asin(cislo), acos(cislo), atan(cislo)
-// * fmod(základ, dělitel) //vrátí zbytek
-// * max(pole), min(pole) // vrátí maximální a minimální hodnotu pole zadaného v parametru.
-// * abs(cislo) //absolutní hodnota zadaného čísla
- //* odečítání
-// * sčítání
-// * dělení
-// * násobení
-// * number format
 ?>
